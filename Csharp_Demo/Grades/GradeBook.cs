8c2206adadec563f20d8bd10de88a1c61f3db47b
Rozita Teymourzadeh <Rozita.Teymourzadeh@neonode.com>
2017-07-05 22:58:15 +0200

    Version 1.0.0.1
    Example 1: Student's grades and statistic

---
+++ Csharp_Demo/Grades/GradeBook.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class GradeBook
+    {
+        public GradeBook()// type ctor (constructor) to get initialize constructor, Only initial costructor does have return type and the name is the same of class
+        {
+            grades = new List<float>();
+        }
+        public GradeStatistics ComputeStatistics()// here return type is "GradeStatistics"
+        {
+            GradeStatistics stats = new GradeStatistics();
+            float sum = 0;
+            foreach(float grade in grades)
+            {
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                sum += grade;
+            }
+            stats.AverageGrade = sum / grades.Count;
+
+            return stats;
+        }
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+        //List<float> grades = new List<float>();// add this line to initialize instructor
+        private List<float> grades;
+    }
+
+    
+}
---
+++ Csharp_Demo/Grades/Program.cs
@@ -0,0 +1,29 @@
+﻿/* This program takes student grades and conduct statistical aanalysis on those grades */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(12.3f);
+            book.AddGrade(18.0f);
+            book.AddGrade(20.0f);
+
+            GradeBook book2 = book; // this type of assignment point to the same address in memory for book2
+
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine(stats.HighestGrade);// snip "cw"
+            Console.WriteLine(stats.LowestGrade);// snip "cw"
+            Console.WriteLine(stats.AverageGrade);// snip "cw"
+            Console.ReadLine();
+        }
+    }
+}
