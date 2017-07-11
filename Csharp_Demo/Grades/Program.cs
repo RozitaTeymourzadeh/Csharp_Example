9e93d5682b97f6df5a3f865a214c84508c0fb79f
Rozita Teymourzadeh <Rozita.Teymourzadeh@neonode.com>
2017-07-11 23:33:06 +0200

    Refactor

--- Csharp_Demo/Grades/Program.cs
+++ Csharp_Demo/Grades/Program.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Speech.Synthesis;
 using System.Text;
@@ -17,8 +18,48 @@ namespace Grades
             synth.Speak("Hello! This program takes student grades and conduct statistical analysis on those grades!Add student grade in main program! here you go!!");
             GradeBook book = new GradeBook();
 
-            book.NameChanged += OnNameChanged;// subscribe to event 
+ 
+            /*Refactor to generate method*/
+            GetBookName(book);
+            AddGrades(book);
+            SaveGrades(book);
+            WriteResults(book);
 
+            /*Experiment*/
+            book.NameChanged += OnNameChanged;// subscribe to event
+            book.Name = "Student Property";
+            book.Name = "Student Property as a book name";
+            GradeBook book2 = book; // this type of assignment point to the same address in memory for book2
+        }
+
+        private static void WriteResults(GradeBook book)
+        {
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine(book.Name);
+            WriteResult("Highest Value", stats.HighestGrade);// snip "cw"
+            WriteResult("Lowest Value", (int)stats.LowestGrade);// snip "cw"
+            WriteResult("Average Value", stats.AverageGrade);// snip "cw"
+            WriteResult(stats.Description, stats.LetterGrade);
+            Console.ReadLine();
+        }
+
+        private static void SaveGrades(GradeBook book)
+        {
+            using (StreamWriter outputFile = File.CreateText("grades.txt"))
+            {
+                book.WriteGrades(outputFile);
+            }
+        }
+
+        private static void AddGrades(GradeBook book)
+        {
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+        }
+
+        private static void GetBookName(GradeBook book)
+        {
             try
             {
                 Console.WriteLine("Enter a Book name");
@@ -32,23 +73,6 @@ namespace Grades
             {
                 Console.WriteLine("Something went wrong!");
             }
-
-            book.Name = "Student Property";
-            book.Name = "Student Property as a book name";
-            book.AddGrade(91);
-            book.AddGrade(89.5f);
-            book.AddGrade(75);
-            book.WriteGrades(Console.Out);
-
-            GradeBook book2 = book; // this type of assignment point to the same address in memory for book2
-
-            GradeStatistics stats = book.ComputeStatistics();
-            Console.WriteLine(book.Name);
-            WriteResult("Highest Value",stats.HighestGrade);// snip "cw"
-            WriteResult("Lowest Value", (int)stats.LowestGrade);// snip "cw"
-            WriteResult("Average Value", stats.AverageGrade);// snip "cw"
-            WriteResult(stats.Description,stats.LetterGrade);
-            Console.ReadLine();
         }
 
         /* Since WriteResult Method is going to be addressed by static Main method , it should be defined as static as well */
